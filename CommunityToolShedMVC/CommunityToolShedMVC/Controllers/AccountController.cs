a0818a53c02adcc601916b713b1d629d683219f0
Omer Latif <olatif@u.rochester.edu>
2019-09-11 12:38:40 -0400

    installed bcrypt

---
+++ CommunityToolShedMVC/CommunityToolShedMVC/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolShedMVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CommunityToolShedMVC.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        // GET: Account
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            var viewModel = new LoginViewModel();
+            return View(viewModel);
+        }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(LoginViewModel viewModel)
+        {
+
+            return View(viewModel);
+        }
+    }
+}
