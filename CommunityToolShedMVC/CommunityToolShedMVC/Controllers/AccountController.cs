6ba584067663f152d05664a0ca2599cfc61599a8
Omer Latif <olatif@u.rochester.edu>
2019-09-11 14:30:56 -0400

    Added Logout

--- CommunityToolShedMVC/CommunityToolShedMVC/Controllers/AccountController.cs
+++ CommunityToolShedMVC/CommunityToolShedMVC/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace CommunityToolShedMVC.Controllers
 {
@@ -23,8 +24,19 @@ namespace CommunityToolShedMVC.Controllers
         [AllowAnonymous]
         public ActionResult Login(LoginViewModel viewModel)
         {
-
+            if (ModelState.IsValid)
+            {
+                FormsAuthentication.SetAuthCookie(viewModel.Email, false);
+                return RedirectToAction("Index", "Home");
+            }
             return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
         }
     }
 }
--- CommunityToolShedMVC/CommunityToolShedMVC/ViewModels/LoginViewModel.cs
+++ CommunityToolShedMVC/CommunityToolShedMVC/ViewModels/LoginViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,10 @@ namespace CommunityToolShedMVC.ViewModels
 {
     public class LoginViewModel
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
